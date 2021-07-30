f5ffb57ab1499a78a8abdcd0f2bf08b39acbd74e
Bruno Garcia <bthemaker@gmail.com>
2021-07-30 13:27:55 -0400

    Initial Commit

---
+++ ZaHunter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ZaHunter
+//
+//  Created by Bruno Garcia on 7/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ZaHunter/ZaHunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZaHunterApp.swift
+//  ZaHunter
+//
+//  Created by Bruno Garcia on 7/30/21.
+//
+
+import SwiftUI
+
+@main
+struct ZaHunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
