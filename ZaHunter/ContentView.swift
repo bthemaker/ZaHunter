5a330a3d267e9a86f7cc16e60ef2dd43c8d89053
Bruno Garcia <bthemaker@gmail.com>
2021-07-30 13:34:23 -0400

    Initial Map Setup

--- ZaHunter/ContentView.swift
+++ ZaHunter/ContentView.swift
@@ -6,11 +6,23 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    
+    @State private var userTrackingMode: MapUserTrackingMode = .follow
+    
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 42.0558,
+            longitude: -87.6743),
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05)
+    )
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Map(coordinateRegion: $region)
     }
 }
 
